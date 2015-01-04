21a56a345267ebc0f9b9a833d1735cd7ac7efa61
Yvette Alasti <yvettecodes@gmail.com>
2015-01-04 01:15:29 -0500

    complete portfolio

---
+++ js/app.js
@@ -0,0 +1,13 @@
+<script type="text/javascript" charset="utf-8">
+    alert($("#fs input.name").val())
+</script>
+<script type="text/javascript" charset="utf-8">
+    alert($("#fs input.email").val())
+</script>
+<script type="text/javascript" charset="utf-8">
+    alert($("#fs input.subject").val())
+</script>
+<script type="text/javascript" charset="utf-8">
+    alert($("#fs input.message").val())
+</script> 
+
